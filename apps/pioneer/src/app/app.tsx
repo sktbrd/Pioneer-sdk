3c2fbf661394fea9365a0d2e55368a9af34c8a54
bithighlander <bithighlander@gmail.com>
2024-03-05 14:19:56 -0700

    lint

--- apps/pioneer/src/app/app.tsx
+++ apps/pioneer/src/app/app.tsx
@@ -37,6 +37,7 @@ export default function App() {
   useEffect(() => {
     onStartApp();
   }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Pioneer />
